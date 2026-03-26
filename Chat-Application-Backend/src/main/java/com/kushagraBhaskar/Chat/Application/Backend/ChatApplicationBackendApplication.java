2a2eb53566a21b17a71acad4d4027e0ddc116915
kushagrabhaskar15 <kushagrabhaskar05@gmail.com>
2026-03-26 21:12:56 +0530

    initial commit

---
+++ Chat-Application-Backend/src/main/java/com/kushagraBhaskar/Chat/Application/Backend/ChatApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kushagraBhaskar.Chat.Application.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplicationBackendApplication.class, args);
+	}
+
+}
---
+++ Chat-Application-Backend/src/test/java/com/kushagraBhaskar/Chat/Application/Backend/ChatApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kushagraBhaskar.Chat.Application.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
